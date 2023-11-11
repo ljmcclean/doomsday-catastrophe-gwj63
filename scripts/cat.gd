112af5f41df6b748f0b4dfc11ecd52832b04ef00
LucksDev <LucksDeveloper@gmail.com>
2023-11-11 08:49:07 +0200

    added simple player movement

---
+++ scripts/cat.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+#Primary Programmer: Lucks
+
+@export var speed : float
+@export var friction : float
+@export var acceleration : float
+
+
+func _physics_process(delta):
+	var direction := Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	direction = direction.normalized()
+	
+	var target_velocity: Vector2 = direction * speed
+#applies friction multiplier rather than acceleration if attempting to change direction of motion
+	if (direction != Vector2.ZERO) and direction.dot(velocity) > 0:
+		velocity = velocity.move_toward(target_velocity, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(target_velocity, friction * delta)
+	
+	move_and_slide()
