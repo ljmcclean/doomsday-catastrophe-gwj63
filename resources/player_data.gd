9bba05a36d6a552dff00300a1acc83e35a98497c
LucksDev <LucksDeveloper@gmail.com>
2023-11-12 11:31:07 +0200

    added player_data and hurtbox

--- resources/player_data.gd
+++ resources/player_data.gd
@@ -1,8 +1,10 @@
-extends Node
+extends Resource
 
 var health : int
 var speed : float
+var is_dead: bool = false
 
-var position : Vector2
+var player_position : Vector2
+var spawn_location := Vector2i(200, 165)
 
 var level_number : int
--- scripts/cat.gd
+++ scripts/cat.gd
@@ -1,12 +1,25 @@
 extends CharacterBody2D
-#Primary Programmer: Daek
+#Primary Programmer: Daek/Lucks
 
 @export var speed : float
 @export var friction : float
 @export var acceleration : float
 
+@export var player_data : Resource
+
+
+func _ready():
+	respawn()
+
 
 func _physics_process(delta):
+	if not player_data.is_dead:
+		move(delta)
+		move_and_slide()
+	update_player_data()
+
+
+func move(delta):
 	var direction := Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
@@ -20,11 +33,31 @@ func _physics_process(delta):
 	else:
 		velocity = velocity.move_toward(target_velocity, friction * delta)
 	
-	move_and_slide()
 	global_rotation = global_position.direction_to(get_global_mouse_position()).angle() + PI/2.0
-	
 
 
 func shoot():
 	pass
-	
+
+
+func _on_hurt_box_area_entered(area):
+	if area.is_in_group("damage_source"):
+		player_data.health -= 1
+
+
+func die():
+	position = Vector2(INF, INF)
+	respawn()
+
+
+func respawn():
+	player_data.health = 5
+	position = player_data.spawn_location
+
+
+#updates player_data resource which can be accessed by other scripts
+func update_player_data():
+	player_data.player_position = self.position
+	if player_data.health <= 0:
+		die()
+
